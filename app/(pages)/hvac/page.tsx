2d5781df1dd05765f62f913e2517c724f91417b7
coding1100 <coding1343453@gmail.com>
2025-11-25 17:04:35 +0500

    deployemnt on VPS

--- app/(pages)/hvac/page.tsx
+++ app/(pages)/hvac/page.tsx
@@ -55,16 +55,12 @@ export default function LandingPage() {
 
       <main className="relative z-10 lg:container lg:mx-auto">
         <div className="sm:px-16 px-9 ">
-          <Hero scrollY={scrollY} />
+          <Hero />
         </div>
         <div className="sm:px-10 px-4">
-          <Features
-            scrollY={scrollY}
-            featureSection={hvacData.featureSection}
-          />
+          <Features featureSection={hvacData.featureSection} />
 
           <PlatformPreviewSection
-            scrollY={scrollY}
             platformPreviewSection={hvacData.platformPreviewSection}
           />
 
--- app/(pages)/plumbing/page.tsx
+++ app/(pages)/plumbing/page.tsx
@@ -55,16 +55,12 @@ export default function LandingPage() {
 
       <main className="relative z-10 lg:container lg:mx-auto">
         <div className="sm:px-16 px-9 ">
-          <Hero scrollY={scrollY} />
+          <Hero />
         </div>
         <div className="sm:px-10 px-4">
-          <Features
-            scrollY={scrollY}
-            featureSection={plumberData.featureSection}
-          />
+          <Features featureSection={plumberData.featureSection} />
 
           <PlatformPreviewSection
-            scrollY={scrollY}
             platformPreviewSection={plumberData.platformPreviewSection}
           />
           <ProcessFlow />
