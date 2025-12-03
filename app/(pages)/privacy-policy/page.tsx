27f195305cf829c7f2674230a658ecbc8f560455
coding1100 <coding1343453@gmail.com>
2025-12-03 18:23:32 +0500

    fixes

--- app/(pages)/privacy-policy/page.tsx
+++ app/(pages)/privacy-policy/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import Footer from "@/app/Components/Footer";
-import NavBar from "@/app/Components/Navbar";
 import PrivacyPolicy from "@/app/Components/PrivacyPolicy";
 
 export default function Page() {
@@ -19,13 +17,9 @@ export default function Page() {
         }}
       />
 
-      {/* <NavBar /> */}
-
       <main className="relative z-10">
         <PrivacyPolicy />
       </main>
-
-      {/* <Footer /> */}
     </div>
   );
 }
